9ee7594ac3e63d831ac5ba1ba1285ece3fc27164
soxand16 <soxand16@aol.com>
2017-12-08 11:18:16 -0600

    modified to test summing an array

--- basic_mp.py
+++ basic_mp.py
@@ -1,13 +1,106 @@
 #!/usr/bin/env python3
 
 import multiprocessing as mp
+import timeit
+import platform
+from matplotlib import pyplot as plt
+import numpy as np
 
-def cube(x) :
-    return {str(x) : x**3}
+def print_sysinfo():
 
-pool = mp.Pool(processes=4)
-results_list = [pool.apply(cube, args=(x,)) for x in range(1,7)] 
-results = {}
-for result in results_list :
-    results = {**results, **result}
-print(results)
+    print('\nPython version  :', platform.python_version())
+    print('compiler        :', platform.python_compiler())
+
+    print('\nsystem     :', platform.system())
+    print('release    :', platform.release())
+    print('machine    :', platform.machine())
+    print('processor  :', platform.processor())
+    print('CPU count  :', mp.cpu_count())
+    print('interpreter:', platform.architecture()[0])
+    print('\n\n')
+
+#def cube(x) :
+#    return {str(x) : x**3}
+#
+#pool = mp.Pool(processes=4)
+#results_list = [pool.apply(cube, args=(x,)) for x in range(1,7)] 
+#results = {}
+#for result in results_list :
+#    results = {**results, **result}
+#print(results)
+
+
+def rsum(a, b) :
+    s = 0
+    for i in range(a,b) :
+        s += i
+    return s
+
+n = 1000000000
+
+def serial(n):
+    print('Running Serial')
+    return rsum(1,n)
+
+def multiprocess(processes, n):
+    print('Running {} processes'.format(processes))
+    r = int(n // processes)
+    l = [(i*r+1, (i+1)*r+1) for i in range(processes)]
+    pool = mp.Pool(processes=processes)
+    data = [pool.apply_async(rsum, args=x) for x in l]
+    data = [p.get() for p in data]
+    results = sum(data)
+    return results
+
+
+# =============================================================================
+# Timing
+# =============================================================================
+
+benchmarks = []
+
+benchmarks.append(timeit.Timer('serial(n)',
+            'from __main__ import serial, n').timeit(number=1))
+
+benchmarks.append(timeit.Timer('multiprocess(2, n)',
+            'from __main__ import multiprocess, n').timeit(number=1))
+
+benchmarks.append(timeit.Timer('multiprocess(3, n)',
+            'from __main__ import multiprocess, n').timeit(number=1))
+
+benchmarks.append(timeit.Timer('multiprocess(4, n)',
+            'from __main__ import multiprocess, n').timeit(number=1))
+
+
+def plot_results():
+    bar_labels = ['serial', '2', '3', '4']
+
+    fig = plt.figure(figsize=(10,6))
+
+    # plot bars
+    y_pos = np.arange(len(benchmarks))
+    plt.yticks(y_pos, bar_labels, fontsize=16)
+    bars = plt.barh(y_pos, benchmarks,
+             align='center', alpha=0.4, color='g')
+
+    # annotation and labels
+
+    for ba,be in zip(bars, benchmarks):
+        plt.text(ba.get_width() + 2, ba.get_y() + ba.get_height()/2,
+                '{0:.2%}'.format(benchmarks[0]/be),
+                ha='center', va='bottom', fontsize=12)
+
+    plt.xlabel('Time, seconds for n=%s' %n, fontsize=14)
+    plt.ylabel('Number of Processes', fontsize=14)
+#    t = plt.title('Serial vs. Multiprocessing via Array Summing Estimation', fontsize=18)
+    plt.ylim([-1,len(benchmarks)])
+    plt.xlim([0,max(benchmarks)*1.1])
+    plt.vlines(benchmarks[0], -1, len(benchmarks)+0.5, linestyles='dashed')
+    plt.grid()
+
+    plt.show()
+    plt.savefig('speed_comparison.png')
+    
+plot_results()
+print_sysinfo()
+
